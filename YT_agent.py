2d61993afbea739aa6ad926236ffe5a3a4a58a02
Raj2002-pro <rajkarmakar180203@gmail.com>
2026-09-08 12:30:35 +0530

    Update YT_agent.py

--- YT_agent.py
+++ YT_agent.py
@@ -1,15 +1,25 @@
+import os
 from textwrap import dedent
 from dotenv import load_dotenv
+import streamlit as st
+
 from agno.agent import Agent
 from agno.models.groq import Groq
 from agno.tools.youtube import YouTubeTools  #we need to do pip install youtube_transcript_api to use this tool
 
 load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")
+
+if not groq_api_key:
+    groq_api_key = st.secrets["GROQ_API_KEY"]
 
 def build_youtube_agent():
     return Agent(
         name="YouTube Agent",
-        model=Groq(id="openai/gpt-oss-20b"),
+        model=Groq(
+            id="openai/gpt-oss-20b",
+            api_key=groq_api_key
+            ),
         tools=[YouTubeTools()],
         instructions=dedent("""\
             You are an expert YouTube content analyst with a keen eye for detail! 🎓
